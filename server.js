3e2e9ef8f220ed7b3b06cf3c8f8720e816c7425f
Prince Carter <princecarter@Princes-MBP.home>
2018-06-17 20:00:32 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+var request = require('request')
+var bodyParser = require('body-parser')
+
+var request = request.defaults({jar: true})
+var j = request.jar()
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+var username;
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+
+  username = req.body.username;
+
+  request.post({url: 'https://gpodder.net/api/2/auth/'+ username +'/login.json', jar: j},function(e,r,body){
+
+  res.redirect('/index.html')
+
+  }).auth(req.body.username, req.body.password, false)
+
+})
+
+// POST /logout gets urlencoded bodies
+app.get('/logout', function (req, res) {
+
+  request.post({url: 'https://gpodder.net/api/2/auth/'+ username +'/logout.json', jar: j},function(e,r,body){
+
+  res.redirect('/login.html')
+
+  })
+
+  console.log(res)
+
+})
+
+
+app.get('/subscriptions', function(req, res){
+
+  request.get({url: 'https://gpodder.net/subscriptions/'+ username +'.json', jar: j}, function(e,r,body){
+
+    res.send(body)
+
+  })
+
+})
+
+app.get('/suggestions', function(req, res){
+
+  request.get({url: 'https://gpodder.net/suggestions/25.json', jar: j}, function(e,r,body){
+
+    console.log(body)
+
+    res.send(body)
+
+  })
+
+})
+
+app.use(express.static('PublicFiles'))
+
+app.listen(8000, () => console.log('Podify running on port 8000!'))
